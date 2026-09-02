9b9c688ce3a92e741ce63fa0fa6916ed4244699d
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-02 23:16:42 +0800

    Refactor Day1 class to Day01 with enhanced comments
    Updated class name and improved comments for clarity.

---
+++ Day01.java
@@ -0,0 +1,19 @@
+public class Day01 {
+    // Komentar untuk satu baris yaitu komentar singkat
+	
+    /* kometar untuk banyak bariss
+          * Fungsi komentar yaitu menjelaskan blok 
+          * dan menonaktifkan kode
+     */
+
+    /**
+          * Komentar untuk dokumentasi atau poin-poin
+          *contoh
+          * @author NamaKamu
+          * @version 1.0
+     */
+    public static void main(String[] args) {
+        System.out.print("belajarrr ngoding");
+    }
+  
+}
