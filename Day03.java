08c9f5fe4bb471c2e87d602655d00aef39aa3798
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-04 22:20:16 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,14 @@
+public class Day03{
+public static void main(String[] args){
+  // \n = New Line atauuu  baris baru
+  System.out.println("Baris 1\nBaris 2\nBaris 3");
+
+  // \t = Tab atauu spasi besar
+  System.out.println("Nama:\tRahmadiah");
+  System.out.println("Hobi:\tMenarii");
+  System.out.println("Alamat:\tpambusuang");
+
+  //  \" kutip ganda di dalam string
+  System.out.println("Dia bilang : \"Mainnnn Yokkkkk\"");
+}
+}
