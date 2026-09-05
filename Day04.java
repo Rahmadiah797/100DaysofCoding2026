a69cb305a41e048612f06954c6c84fc41e3b66f6
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-05 22:42:29 +0800

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,22 @@
+public class Day04{
+    
+    public static void main(String[] args) {
+        
+        //byte = paling kecil hanya bisa -128 sampai 127
+        byte umur = 20; 
+        System.out.println("Byte Umur: " + umur);
+        
+        //short =  hanya bisaaa-32,768 sampai 32,767
+        short tahun = 2026;
+        System.out.println("Short Tahun: " + tahun);
+        
+        // int = paling sering dipakai kapasitas angkanya yaitu -2 Miliar sampai 2 Miliar
+        int jumlahPenduduk = 1500000;
+        System.out.println("Int Penduduk: " + jumlahPenduduk);
+        
+        //long = paling besar Tambah L di belakang angka
+        long jarakBumiBulan = 384400000L; // L wajib kalau angkanya besar
+        System.out.println("Long Jarak: " + jarakBumiBulan + " meter");    
+        
+    }
+}
