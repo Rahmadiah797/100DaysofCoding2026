93f61d30265f1d29a975c29fb5f0af4030dab46e
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-06 23:15:25 +0800

    Create Day05 class to demonstrate float and double
    Added a class Day05 with main method demonstrating float and double types.

---
+++ Day05.java
@@ -0,0 +1,18 @@
+public class Day05{
+    
+    public static void main(String[] args) {
+        
+        // float = 7 angka di belakang koma dn WAJIB ada F di belakang angka
+        float phi = 3.14F; 
+        float berat = 55.5F;
+        System.out.println("Float Phi: " + phi);
+        System.out.println("Float Berat: " + berat + " kg");
+        
+        //double = 15 angka di belakang koma dan Defaultnya Java. Boleh pakai D atau tanpa D
+        double pi = 3.141592653589793; 
+        double harga = 15000.75;
+        System.out.println("Double Pi: " + pi);
+        System.out.println("Double Harga: Rp " + harga);
+
+    }
+}
