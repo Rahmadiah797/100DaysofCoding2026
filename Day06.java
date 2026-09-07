c67cc29be05a749bcca52b5bc157fe893d2282b6
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-07 22:55:32 +0800

    Add Day06 class with main method and variables

---
+++ Day06.java
@@ -0,0 +1,11 @@
+public class Day06{
+    public static void main(String[] args) {
+        
+        char nilai = 'A';     // char = 1 karakter, pakai ' '
+        boolean lulus = true; // boolean = true atau false
+        
+      //menampilkan
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Lulus: " + lulus);
+    }
+}
