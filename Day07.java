89741bba5bab2a4c0ec8d5da0b3eda78dd87267a
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-08 23:14:09 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,8 @@
+public class Day07{
+  public static void main(String[] args) {
+    //type data string = type data non primitif
+    // jdi mnggunakan huruf besar pda awal huruf
+  String nama = "Rahmadiah";
+  System.out.println("nama    = " + nama);
+  }
+}
