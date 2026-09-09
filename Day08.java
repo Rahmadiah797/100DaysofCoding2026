9e24d053b70b983ba64b65a5ca24114790816082
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-09 22:31:45 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,18 @@
+public class Day08{
+    public static void main(String[] args) {
+        
+        //DEKLARASI
+        int umur;
+        
+        // INISIALISASI
+        umur = 18;
+        System.out.println("Sebelum Update");
+        System.out.println("umur: " + umur);
+
+      
+        // UPDATE
+        umur = 20;
+        System.out.println("\nSetelah Update:");
+        System.out.println("Umur: " + umur);
+    }
+}
