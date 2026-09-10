315238ef38f18cd6e2156c82b863ba4bbddca967
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-10 20:24:44 +0800

    Create Day09.java

---
+++ Day09.java
@@ -0,0 +1,12 @@
+public class Day09{
+    public static void main(String[] args) {
+         //Constanta final = nilainya tidak bisa diubahh
+        final int UMUR_DEWASA = 18;
+
+        //ini yang bisa terubah
+        int umurSaya = 17;
+        
+        System.out.println("Umur saya: " + umurSaya);
+        System.out.println("Batas dewasa: " + UMUR_DEWASA);
+    }
+}
