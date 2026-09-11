034ba37099d70c8c60c6af368bf13359ca60227c
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-11 21:52:54 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,30 @@
+public class Days10 {
+    public static void main(String[] args) {
+    
+        // INT
+        // int = tipe data bilangan bulat 32 bit. Paling sering dipakai
+        System.out.println("\nINT");
+        System.out.println("Minimal  : " + Integer.MIN_VALUE); 
+        System.out.println("Maksimal : " + Integer.MAX_VALUE); 
+
+        // SHORT
+        // short = tipe data bilangan bulat 16 bit. Range: -32.768 sampai 32.767
+        System.out.println("\nSHORT");
+        System.out.println("Minimal  : " + Short.MIN_VALUE); 
+        System.out.println("Maksimal : " + Short.MAX_VALUE); 
+
+        // BYTE
+        // byte = tipe data bilangan bulat 8 bit. Range: -128 sampai 127
+        System.out.println("\nBYTE");
+        System.out.println("Minimal  : " + Byte.MIN_VALUE);  
+        System.out.println("Maksimal : " + Byte.MAX_VALUE);  
+
+        // LONG
+        // long = tipe data bilangan bulat 64 bit. Untuk angka sangat besar
+        System.out.println("\nLONG");
+        System.out.println("Minimal  : " + Long.MIN_VALUE); 
+        System.out.println("Maksimal : " + Long.MAX_VALUE); 
+
+    
+    }
+}
