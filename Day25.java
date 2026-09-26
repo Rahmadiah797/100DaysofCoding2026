7ce039edc92df44c3afcb326b5a55516f38771ff
Rahmadiah797 <rhmadiah00@gmail.com>
2026-09-26 23:14:15 +0800

    Add program to calculate circle area

---
+++ Day25.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        double r = input.nextDouble();
+
+        double luas = Math.PI * r * r;
+
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
